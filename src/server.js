83cfaf943746402d15562e452af71f6b51c48ff1
phinahasWazeefa <phinahas@wazeefa.in>
2024-05-20 16:58:32 +0530

    teting by changing route setting

--- src/server.js
+++ src/server.js
@@ -22,21 +22,21 @@ server.use('/test',(req,res,next)=>{
 })
 
 
-//Handling invalid api request
-server.use((req, res, next) => {
-    const err = new Error("Not Found");
-    err.status = 404 || err.status;
-    res.status(err.status).send({ message: "Invalid API" });
-    next();
-  });
+// //Handling invalid api request
+// server.use((req, res, next) => {
+//     const err = new Error("Not Found");
+//     err.status = 404 || err.status;
+//     res.status(err.status).send({ message: "Invalid API" });
+//     next();
+//   });
 
-  //Hadeling error and sending response
-server.use((error, req, res, next) => {
-  const statusCode = error.status || 500;
-  const message = error.message;
-  console.log(error);
-  res.status(statusCode).json({ data: { message } });
-});
+//   //Hadeling error and sending response
+// server.use((error, req, res, next) => {
+//   const statusCode = error.status || 500;
+//   const message = error.message;
+//   console.log(error);
+//   res.status(statusCode).json({ data: { message } });
+// });
 
   // dbConnnection.dbConnection.then(()=>{
    
