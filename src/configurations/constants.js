80a2d052c6b03846ad01f9e0571b64cfe2141180
phinahasWazeefa <phinahas@wazeefa.in>
2025-08-31 13:39:46 +0530

    DB connection changed to ritzmotoclub account

--- src/configurations/constants.js
+++ src/configurations/constants.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-    DB_URL : process.env.DB_URI || `mongodb+srv://phinahas:phinahasphilip@my-cluster.udgav8g.mongodb.net/RMC-TEST?retryWrites=true&w=majority&appName=My-Cluster`,
+    DB_URL : process.env.DB_URI || `mongodb+srv://hello_db_user:kQdjQTAcN6FpUY8e@rmc-free-cluster.1ujtfpu.mongodb.net/RMCretryWrites=true&w=majority&appName=RMC-Free-Cluster`,
     PORT:process.env.PORT || 8080,
     JWT_SECRET_KEY:process.env.JWT_SECRET_KEY || '10958@Phinahas',
     MAIL_HOST:process.env.MAIL_HOST||'mail.ritzmotoclub.com'  ,
